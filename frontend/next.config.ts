d023299352c551e0a6d49abf4798432be3554914
Feiran Zhang <feiranzhang@UWCCSCMAC011.local>
2026-05-15 16:37:07 +0800

    web development #4

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: 'CoTEACH/frontend',  // your repo name
+  basePath: '/CoTEACH',  // your repo name
   images: { unoptimized: true },
   env: {
     NEXT_PUBLIC_API_URL: 'https://coteach-2q9m.onrender.com/'  // Render URL
